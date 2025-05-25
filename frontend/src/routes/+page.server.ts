a94e7be0b9d625105627d9f80fd742e927f2ab00
Blooym <git@blooym.dev>
2025-05-26 00:43:40 +0100

    feat: alias xml as html in syntax list

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -2,10 +2,30 @@ import { HighlighterLanguages, type HighlighterLanguageKey } from '$lib/highligh
 import hljs from 'highlight.js';
 import type { PageServerLoad } from './$types';
 
-const highlightLanguages = hljs
-    .listLanguages()
-    .filter((lang): lang is HighlighterLanguageKey => lang in HighlighterLanguages)
-    .sort();
+type HighlighterLanguage = {
+    name: HighlighterLanguageKey;
+    alias: string | null;
+};
+
+const highlightAliases: HighlighterLanguage[] = [
+    {
+        name: 'xml',
+        alias: 'html'
+    }
+];
+
+const highlightLanguages: HighlighterLanguage[] = [
+    ...hljs
+        .listLanguages()
+        .filter((lang): lang is HighlighterLanguageKey => lang in HighlighterLanguages)
+        .map((lang) => ({
+            name: lang,
+            alias: null
+        })),
+    ...highlightAliases
+].sort((a, b) =>
+    (a.alias ?? a.name).toLowerCase().localeCompare((b.alias ?? b.name).toLowerCase())
+);
 
 export const load: PageServerLoad = async ({ locals }) => {
     return {
